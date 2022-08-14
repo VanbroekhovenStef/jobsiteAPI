8ccff7c2ef51592e4f0759288213016c023bb052
VanbroekhovenStef <r0630975@student.thomasmore.be>
2022-08-14 20:29:17 +0200

    Aanpassingen feedback Michael

--- Jobsite.API/Controllers/UsersController.cs
+++ Jobsite.API/Controllers/UsersController.cs
@@ -93,7 +93,6 @@ namespace Jobsite.API.Controllers
 
         // POST: api/Users
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [Authorize]
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
